ab0cd254756065c8870cf7ef5a6d939b3a0ca343
shikhar <shikharagarwale7@gmail.com>
2023-05-20 22:39:52 +0530

    added search-insert-pos

---
+++ Binary Search/Search-Insert-Position.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+
+class Solution {
+public:
+    int searchInsert(std::vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (target > nums[mid]) {
+                start = mid + 1;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        if (target < nums[end]) {
+            return end;
+        } else {
+            return end + 1;
+        }
+    }
+};
+
+
+// Uncomment it to run it on your system
+// int main() {
+//     Solution myclass;
+//     std::vector<int> nums = {1, 3, 5, 6};
+//     int target = 6;
+//     int result = myclass.searchInsert(nums, target);
+//     std::cout << result << std::endl;
+
+//     return 0;
+// }
